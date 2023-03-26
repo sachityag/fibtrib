59131fb01eccc88a0874386e0c1fdd6aab3d4071
sachityag <sachityagi0807@gmail.com>
2023-03-26 16:49:22 -0700

    working tribonacci files and function

--- tribonacci.c
+++ tribonacci.c
@@ -2,13 +2,25 @@
 #include "tribonacci.h"
 
 
-int trib(int fn)
+int trib(int n)
 {
-	int i, n;
-	int fn1 = 1;
-	int fn2 = 1;
-	int fn3 = 1;
-	fn = fn3 + fn2 + fn1;
-	return 0;
+	int i, tn;
+	int tn1 = 1;
+	int tn2 = 1;
+	int tn3 = 1;
+	tn = tn3 + tn2 + tn1;
+	if(n <= 3) {
+		return 1; 
+	}
+	if(n == 4) { 
+		return tn;
+	} 
+	for(i=4;i<n;i++) {
+		tn3 = tn2;
+		tn2 = tn1;
+		tn1 = tn;
+		tn = tn3 + tn2 + tn1;
+	}
+	return tn;
 }
 
--- tribtest.c
+++ tribtest.c
@@ -6,5 +6,16 @@
 
 int main(int argc, char *argv[])
 {
+	int i, n, tn;
+	if(argc == 2) {
+		n = atoi(argv[1]);
+	}
+	else {
+		printf("bad input\n");
+		return 0;
+	}
+	tn = trib (n);
+	printf("Entered term number: %d\n", n);
+	printf("Tribonacci number is: %d\n", tn);
 	return 0;
 }
