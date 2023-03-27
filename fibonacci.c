b31e757d5d931d0e636cefca95c02e6df1751b29
syontyagi <syontyagi1109@gmail.com>
2023-03-26 17:04:56 -0700

    completed fibtest.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "fibonacci.h"
 
-int fib(int fn)
+int fib (int n)
 {
-	int i, n;
+	int i, fn;
 	int fn1 = 1;
 	int fn2 = 1;
-	 fn = fn1 + fn2;
+	fn = fn1 + fn2;
+	if (n <= 2) {
+		return 1;
+	}
+	if (n == 3) {
+		return fn;
+	}
 	for (i=3;i<n;i++) {
-		fn = fn2 + fn1;
 		fn2 = fn1;
 		fn1 = fn;
+		fn = fn2 + fn1;
 	}
-	return 0;
+	return fn;
 }
--- fibtest.c
+++ fibtest.c
@@ -4,5 +4,16 @@
 
 int main(int argc, char *argv[])
 {
+	int i, n, fn;
+	if(argc == 2) {
+		n = atoi(argv[1]);
+	}
+	else {
+		printf("bad input\n");
+		return 0;
+	}
+	fn = fib (n);
+	printf("Entered term number: %d\n", n);
+	printf("Fibonacci number is: %d\n", fn);
 	return 0;
 }
